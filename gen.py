d2fbdd6dabbab947882c12603c6d198d14be8924
raymund.rimando <raymund.rimando@gmail.com>
2023-05-24 13:53:47 -0400

    Add futures support

--- gen.py
+++ gen.py
@@ -4,19 +4,38 @@ import argparse
 WINDOWS_LINE_ENDING = '\r\n'
 UNIX_LINE_ENDING = '\n'
 
+FUTURES_FACTOR = {
+    'ES': 50,
+    'MES': 5,
+    'NQ': 20,
+    'MNQ': 0.50
+}
+
 parser = argparse.ArgumentParser()
 parser.add_argument('risk', type=int, help='default amount to risk($)')
 parser.add_argument('--account', type=int, default=25000, help='Account size($25000)')
 parser.add_argument('--template', '-T', type=str, default="default.htk.j2", help='default template file')
 parser.add_argument('--output', '-o', type=str, default='/dev/stdout', help='Output file')
+parser.add_argument('--npt', '-n', type=int, default=3, help='Number of profit targets')
+parser.add_argument('--nhk', '-k', type=int, default=6, help='Number of hotkey targets')
+parser.add_argument('--pto', '-O', type=float, default=0.20, help='Profit target offset')
+parser.add_argument('--futures', choices=['ES', 'MES', 'NQ', 'MNQ'], default=None, help='Futures type')
 args = parser.parse_args()
 
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template(args.template)
 
+
+if args.futures is not None:
+    args.risk = (int)(args.risk/FUTURES_FACTOR[args.futures])
+
 content = template.render(
         risk=args.risk,
-        account=args.account
+        account=args.account,
+        npt=args.npt,
+        pto=args.pto,
+        nhk=args.nhk,
+        future=FUTURES_FACTOR.get(args.futures, 1)
     )
 
 dos = content.replace('\n', '\r\n')
--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-for i in 50 100 150 200 250 300;
+for i in 50 100 150 200 250 300 350 400 450 500;
 do 
-  python3 gen.py --account 25000 -o /Volumes/\[C\]\ Windows\ 11/DAS\ Trader\ Pro/hotkey-$i.htk $i
+  python3 gen.py --account 25000 -o /Volumes/\[C\]\ Windows\ 11/DAS\ Trader\ Pro/hotkeys/hotkey-$i.htk $i
 done
+
+python3 gen.py --account 25000 --future MES -o /Volumes/\[C\]\ Windows\ 11/DAS\ Trader\ Pro/hotkeys/hotkey-MES-125.htk 125
+python3 gen.py --account 25000 --future ES -o /Volumes/\[C\]\ Windows\ 11/DAS\ Trader\ Pro/hotkeys/hotkey-ES-250.htk 250
+
+python3 gen.py --account 25000 --future MNQ -o /Volumes/\[C\]\ Windows\ 11/DAS\ Trader\ Pro/hotkeys/hotkey-MNQ-25.htk 25
+python3 gen.py --account 25000 --future NQ -o /Volumes/\[C\]\ Windows\ 11/DAS\ Trader\ Pro/hotkeys/hotkey-NQ-100.htk 100
